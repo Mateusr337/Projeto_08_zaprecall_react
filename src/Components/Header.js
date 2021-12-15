4e7cd511329a5333e82a67b4aa880ff506db2b1d
leo <celsolnv27@gmail.com>
2021-12-15 17:21:48 -0300

    Preparando revisão de código

--- src/Components/Header.js
+++
@@ -1,8 +0,0 @@
-import LogoMini from '../assets/logo-mini.png';
-
-export default function Header(){
-    return(
-        <div className="header">
-            <img src={LogoMini} alt='logo'/>
-        </div>
-    )}
